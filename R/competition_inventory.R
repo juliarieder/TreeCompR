e691a6f5b8f50cb8a4dfd752a3ab7e1780f42da9
Julia Rieder <julia.rieder@uni-wuerzburg.de>
2023-10-10 16:20:50 +0200

    new tests, debugging function

--- R/competition_inventory.R
+++ R/competition_inventory.R
@@ -13,10 +13,10 @@
 #' @importFrom utils data
 #' @importFrom data.table fread
 #' @importFrom rlang .data
-#' @importFrom dplyr full_join
 #' @importFrom dplyr filter
 #' @importFrom dplyr group_by
-#' @importFrom dplyr summarize
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
 #' @export
 #'
 #' @examples
@@ -34,13 +34,15 @@ compete_calc <- function(path, radius = 10, dbh_thr = 0.1, target_tree, type = c
 
   #Meldung einbauen, falls invalid extension
   trees <- data.table::fread(path)
-  trees <- data.frame(trees[, 1:3])
+  trees <- data.frame(trees[, 1:5])
+  target_tree <- as.numeric(target_tree)
   colnames(trees) <- c("ID", "X", "Y", "DBH", "H")
 
   if (type == "ID") {
     #user provides the ID of the target tree
     #Look up the right coordinates
-    matching_rows <- trees[.data$ID == target_tree]
+    #matching_rows <- trees[trees$ID == target_tree]
+    matching_rows <- subset(trees, trees$ID == target_tree)
     if (nrow(matching_rows) == 0){
       stop("This Tree ID is not existing within this plot!")
     } else if(nrow(matching_rows) > 1) {
@@ -59,29 +61,42 @@ compete_calc <- function(path, radius = 10, dbh_thr = 0.1, target_tree, type = c
       } else {
 
       }}}
-  return(trees)
-  trees <- trees %>% dplyr::filter(.data$DBH >= dbh_thr)
-  tree <- trees %>% dplyr::filter(.data$status == "target_tree") %>% mutate(H_target = .data$H, dbh_target = .data$DBH, X_target = .data$X, Y_target = .data$Y) %>% dplyr::select(.data$H_target, .data$dbh_target, .data$X_target, .data$Y_target)
-  competition <- dplyr::full_join(trees, tree) %>% mutate(euc_dist_comp = sqrt((.data$X_target - .data$X)^2 + (.data$Y_target - .data$Y)^2)) %>% dplyr::filter(.data$euc_dist_comp <= radius) %>%
+  trees <- trees %>% dplyr::mutate(
+    H_target = matching_rows$H,
+    dbh_target = matching_rows$DBH,
+    X_target = matching_rows$X,
+    Y_target = matching_rows$Y,
+    target_ID = matching_rows$ID)
+
+  trees <- trees %>% dplyr::mutate(euc_dist_comp = sqrt((.data$X_target - .data$X)^2 + (.data$Y_target - .data$Y)^2)) %>% dplyr::filter(.data$euc_dist_comp <= radius) %>%
     dplyr::mutate(CI_h_part = .data$DBH / (.data$dbh_target * .data$euc_dist_comp)) %>%
     dplyr::mutate(CI12_part = atan(.data$H / .data$euc_dist_comp), CI13_part = (.data$H / .data$H_target) * atan(.data$H / .data$euc_dist_comp)) %>% dplyr::mutate(CI11_part = (.data$DBH / .data$dbh_target) * atan(.data$DBH / .data$euc_dist_comp)) %>%
-    dplyr::filter(.data$status == "competitor") %>% dplyr::mutate(target_ID = tree$ID)
-  CIs <- competition %>% dplyr::group_by(.data$target_ID) %>% dplyr::summarise(sum(.data$CI_h_part), sum(.data$CI11_part), sum(.data$CI12_part), sum(.data$CI13_part)) # Tree ID noch einbinden! oder anders vorgehen
-  names(CIs) <- c('target_ID', 'CI_Hegyi', 'CI11', 'CI12', 'CI13')
-  #Hegyi, C11, C12, C13...
-  if (method == "all"){
+    dplyr::filter(.data$status == "competitor")
+
+  CIs <- trees %>% dplyr::group_by(.data$target_ID) %>% dplyr::summarise(
+    CI_Hegyi = sum(.data$CI_h_part),
+    CI11 = sum(.data$CI11_part),
+    CI12 = sum(.data$CI12_part),
+    CI13 = sum(.data$CI13_part)
+  )
+
+  if (method == "all") {
     return(CIs)
-  } else if (method == "Hegyi"){
-    CI_Hegyi <- CIs %>% dplyr::select(.data$target_ID, .data$CI_Hegyi)
+  } else if (method == "Hegyi") {
+    CI_Hegyi <- CIs %>%
+      dplyr::select(.data$target_ID, .data$CI_Hegyi)
     return(CI_Hegyi)
-  } else if (method == "CI11"){
-    CI_11 <- CIs %>% dplyr::select(.data$target_ID, .data$CI11)
+  } else if (method == "CI11") {
+    CI_11 <- CIs %>%
+      dplyr::select(.data$target_ID, .data$CI11)
     return(CI_11)
-  } else if (method == "CI12"){
-    CI_12 <- CIs %>% dplyr::select(.data$target_ID, .data$CI12)
+  } else if (method == "CI12") {
+    CI_12 <- CIs %>%
+      dplyr::select(.data$target_ID, .data$CI12)
     return(CI_12)
-  } else if (method == "CI13"){
-    CI_13 <- CIs %>% dplyr::select(.data$target_ID, .data$CI13)
+  } else if (method == "CI13") {
+    CI_13 <- CIs %>%
+      dplyr::select(.data$target_ID, .data$CI13)
     return(CI_13)
   } else {
     stop("This input format is not supported, please enter an existing Tree ID or the coordinates of the target tree.")
--- tests/testthat/test-competition.R
+++ tests/testthat/test-competition.R
@@ -9,3 +9,16 @@ test_that("quantify competition (cylinder) for .txt file point clouds works", {
     competition_pc(forest_path = "../data/neighborhood.txt",  tree_path = "../data/tree.txt", comp_method = "cylinder", cyl_r = 4)),
     3)
 })
+
+test_that("quantify competition (cylinder) for .txt file point clouds works with customized radius", {
+  expect_equal(length(
+    competition_pc(forest_path = "../data/neighborhood.txt",  tree_path = "../data/tree.txt", comp_method = "cylinder", cyl_r = 3)),
+    3)
+})
+
+#compete_calc <- function(path, radius = 10, dbh_thr = 0.1, target_tree, type = c("ID", "coordinates"), tolerance = 0.1, method = c("all", "Hegyi", "CI12", "CI13")
+test_that("Hegyi index works", {
+  expect_equal(length(
+    compete_calc(path = "../data/inventory.csv",  radius = 10, dbh_thr = 0.1, target_tree = 14, type = "ID", method = "Hegyi")),
+    2)
+})
