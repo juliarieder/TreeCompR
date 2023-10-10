5f508f16680f2f4bea9917181e3bbbac14d371f4
Julia Rieder <julia.rieder@uni-wuerzburg.de>
2023-10-10 10:48:49 +0200

    commit RStudio

--- R/read_tree.R
+++ R/read_tree.R
@@ -3,6 +3,9 @@
 #' @param path character path to point cloud of individual tree or a whole plot
 #'
 #' @return data frame with X,Y,Z of the tree point cloud
+#'
+#' @importFrom data.table fread
+#' @importFrom utils data
 #' @export
 #'
 #' @examples
@@ -11,8 +14,8 @@
 #' tree <- read_tree(path = "path/to/tree_point_cloud.txt")
 #' }
 read_tree <- function(path){
-data <- data.table::fread(path)
-tree <- data.frame(data[, 1:3])
-colnames(tree) <- c("X", "Y", "Z")
-return(tree)
+  data <- data.table::fread(path)
+  tree <- data.frame(data[, 1:3])
+  colnames(tree) <- c("X", "Y", "Z")
+  return(tree)
 }
