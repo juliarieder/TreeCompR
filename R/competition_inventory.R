c1fbd1a8d1e07b67b8ab425cd8a5898042cf9379
Julia Rieder <julia.rieder@uni-wuerzburg.de>
2023-10-17 17:45:51 +0200

    function descriptions

--- R/competition_inventory.R
+++ R/competition_inventory.R
@@ -15,7 +15,7 @@
 #'  * CI13 according to Rouvinen & Kuuluvainen (1997)
 #'  ...
 #'
-#'  @section Literature:
+#' @section Literature:
 #'
 #'  * Hegyi, F., 1974. A simulation model for managing jackpine stands. In: Fries, J. (Ed.), Proceedings of IUFRO meeting S4.01.04 on Growth models for tree and stand simulation, Royal College of Forestry, Stockolm.
 #'  * Rouvinen, S., Kuuluvainen, T., 1997. Structure and asymmetry of tree crowns in relation to local competition in a natural mature Scot pine forest. Can. J. For. Res. 27, 890–902.
