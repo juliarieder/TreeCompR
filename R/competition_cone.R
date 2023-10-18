4209e3664d103bc639aaedc73d37b27167cbc8c8
Julia Rieder <julia.rieder@uni-wuerzburg.de>
2023-10-18 14:52:16 +0200

    Documentation

--- R/competition_cone.R
+++ R/competition_cone.R
@@ -1,11 +1,22 @@
-#' Quantify Tree Crown Competition (Cone Method or cylinder method)
-#' @description opening angle 60°, spanned in 50 % of target tree's height
+#' Quantify Tree Competition from Point Clouds (Cone or cylinder method)
+#' @description Counting the points or voxels of neighboring trees that reach into search cone or cylinder for the target tree
 #' @param forest_path character path to file of neighborhood point cloud (including the target tree and neighbors, not height normalized, can include ground points)
 #' @param tree_path character path to file of target tree point cloud
 #' @param comp_method character string with competition method "cone" or "cylinder"
 #' @param cyl_r (Optional) only important when using comp_method "cylinder"; numeric value of cylinder radius in m. Default is 4 m.
 #'
 #' @return vector of counted points and voxels of neighborhood point cloud that reach into the cone spanned over the target tree
+#'
+#' @details
+#' * Cone Method: search cone with opening angle 60°, spanned in 50 % of target tree's height, counting the points and voxels of neighboring trees that reach within
+#' * Cylinder Method: search cylinder with specific radius (target tree in center), counting the points and voxels of neighboring trees that reach within
+#'
+#' Check the Literature:
+#' * Metz, J., Seidel, D., Schall, P., Scheffer, D., Schulze, E.-D. & Ammer, C. (2013). Crown modeling by terrestrial laser scanning
+#' as an approach to assess the effect of aboveground intra- and interspecific competition on tree growth. Forest Ecology and Management,
+#' 310: 275-288. https://doi.org/10.1016/j.foreco.2013.08.014
+#' * Seidel, D., Hoffmann, N., Ehbrecht, M., Juchheim, J. & Ammer, C. (2015). How neighborhood affects tree diameter increment – New insights from
+#' terrestrial laser scanning and some methodical considerations. Forest Ecology and Management, 336: 119-128. http://dx.doi.org/10.1016/j.foreco.2014.10.020
 #'
 #' @export
 #'
