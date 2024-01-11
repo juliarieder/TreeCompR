0ea70070799883b0b2c6843d1f401a09cd10cb3c
Julia Rieder <julia.rieder@uni-wuerzburg.de>
2024-01-11 10:01:50 +0100

    updating CI names

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("CI11", "CI11_part", "CI12", "CI12_part", "CI13", "CI13_part", "CI_h_part",
+utils::globalVariables(c("CI_h_part","CI_dd2_part", "CI_dd3_part", "CI_hd1_part", "CI_hd2_part", "CI_hd3_part",
                          "DBH", "H", "ID", "X", "Y", "Z", "dist", "euc_dist", "euc_dist_comp",
                          "r_cone", "status", ".", "H_target", "ID_t", "ID_target", "X_seg", "Y_seg",
-                         "X_segt", "Y_segt", "dbh_target", "CI10", "CI10_part", "CI_10"))
+                         "X_segt", "Y_segt", "dbh_target"))
