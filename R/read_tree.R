08277755f17b6ca49637c7fcf5c05819eb4d12cb
Roman Link <romanlink@gmx.de>
2024-03-04 17:18:20 +0100

    Add class check to .validate_tree & convert coordinates to lower case

--- R/read_tree.R
+++ R/read_tree.R
@@ -22,11 +22,11 @@
 #'   vectors.
 #'   If no columns with matching names are available, it takes the first three
 #'   numeric columns and returns a message. If the dataset does not contain
-#'   three numeric columns or one of the columns labeled X, Y, and Z is not
+#'   three numeric columns or one of the columns labeled x, y, and z is not
 #'   numeric, the function fails with an error.
 #'
-#' @return data.frame with X,Y and Z coordinates of the tree or forest point
-#'    cloud
+#' @return object of class c("tree_pc", "data.frame") with x, y and z
+#'  coordinates of the tree or forest point cloud
 #' @export
 #'
 #' @examples
@@ -42,25 +42,29 @@ read_tree <- function(tree_source, ...) {
   if (inherits(tree_source, "data.frame")){
     tree <- .validate_tree(tree_source)
   } else if (!(is.character(tree_source) && length(tree_source) == 1)){
-    stop("Format of tree_source not recognized. \nPlease provide a data.frame or a path to a source file.\n")
+    stop("Format of tree_source not recognized.\n",
+         " Please provide a data.frame or a path to a source file.\n")
   } else if(!file.exists(tree_source)){
-    stop("File", tree_source, "does not exist. \nPlease check path to point cloud file.")
+    stop("File", tree_source,
+         "does not exist. \nPlease check path to point cloud file.")
   } else {
     # treat tree_source as path to file
     path <- tree_source
     # get file extension
-    extension <- utils::tail(base::strsplit(path, split = ".", fixed = TRUE)[[1]], 1)
+    extension <- utils::tail(
+      base::strsplit(path, split = ".", fixed = TRUE)[[1]], 1)
     # load las/laz point cloud
     if (extension %in% c("las", "laz")) {
       # Check if lidR package is installed
       if (requireNamespace("lidR", quietly = TRUE)) {
         # If installed, proceed with the code for *.las files
         las <- lidR::readTLSLAS(path)
-        tree <- data.frame("X" = las$X, "Y" = las$Y, "Z" = las$Z) %>%
+        tree <- data.frame(x = las$X, y = las$Y, z = las$Z) %>%
           .validate_tree()
       } else {
         # If not, return an error message about the missing package
-        stop("Please install the 'lidR' package if you want to use data in las/laz format. \n")
+        stop("Please install the 'lidR' package",
+             " if you want to use data in las/laz format. \n")
       }
     } else {
       # try loading in point cloud with fread
@@ -69,47 +73,59 @@ read_tree <- function(tree_source, ...) {
       )
       if (inherits(tree, "try-error")) {
         # if the file cannot be read, return error message about accepted formats.
-        stop("File format cannot be read. Please use point cloud with extension las/laz\n or a format readable by data.table::fread().")
-      } else{
-       tree <- .validate_tree(tree)
+        stop("File format cannot be read. ",
+             "Please use point cloud with extension las/laz",
+             "\n or a format readable by data.table::fread().")
+      } else{ # else validate and return
+        tree <- .validate_tree(tree)
       }
     }
   }
-  # return object with correct column number, names and types
+  # return tree_df object with correct column number, names and types
   return(tree)
 }
 
 #' @keywords internal
 #' internal function for the validation of point cloud data
 .validate_tree <- function(tree){
-  # check for consistency
-  if (all(c("X", "Y", "Z") %in% toupper(names(tree)))){ # if coordinates are named, use these
-    # convert to upper case
-    names(tree) <- toupper(names(tree))
-    # use these columns
-    tree <- tree[, c("X", "Y", "Z")]
-    # test if any of the columns has the wrong class
-    if (!all(sapply(tree, is.numeric))){
-      stop("One or more of the coordinate vectors X, Y, Z is not numeric.\nPlease check raw data.")
+  # check if tree is already formatted correctly and return if true
+  if (inherits(tree, "tree_pc")){
+    return(tree)
+  } else { #else check for consistency
+    # if coordinates are named, use these
+    if (all(c("x", "y", "z") %in% tolower(names(tree)))){
+      # convert to upper case
+      names(tree) <- tolower(names(tree))
+      # use these columns
+      tree <- tree[, c("x", "y", "z")]
+      # test if any of the columns has the wrong class
+      if (!all(sapply(tree, is.numeric))){
+        stop("One or more of the coordinate vectors x, y, z",
+             " is not numeric.\nPlease check raw data.")
+      }
+    } else{ # else, use the first three numeric columns
+      # get numeric columns
+      nums <- which(sapply(tree, is.numeric))
+      # warn if there are not enough numeric columns
+      if (length(nums) < 3){
+        stop("Tree contains less than 3 numeric coordinate vectors.",
+             "\n Please check raw data.")
+      } else {
+        # get first three numeric columns
+        tree <- tree[,nums[1:3]]
+        # message about used coordinate vectors
+        message(
+          "No named coordinates. Columns ",
+          paste(names(tree), collapse = ", "),
+          " (no. ", paste(nums[1:3], collapse = ", "),
+          " in raw data)\n   used as x, y, z coordinates, respectively.")
+        # adjust names
+        names(tree) <- c("x", "y", "z")
+      }
     }
-  } else{ # else, use the first three numeric columns
-    # get numeric columns
-    nums <- which(sapply(tree, is.numeric))
-    # warn if there are not enough numeric columns
-    if (length(nums) < 3){
-      stop("Tree contains less than 3 numeric coordinate vectors.\nPlease check raw data.")
-    } else {
-      # get first three numeric columns
-      tree <- tree[,nums[1:3]]
-      # message about used coordinate vectors
-      message(paste0("No named coordinates. Columns ",
-                     paste(names(tree), collapse = ", "),
-                     " (no. ", paste(nums[1:3], collapse = ", "),
-                     " in raw data)\n   used as X, Y, Z coordinates, respectively."))
-      # adjust names
-      names(tree) <- c("X", "Y", "Z")
-    }
+    # set class to tree_pc object
+    class(tree) <- c("tree_pc", class(tree))
+    # return the validated tree object
+    return(tree)
   }
-  # return the validated tree object
-  return(tree)
 }
