3d552a15e74acebec91ee64aeee975913b8cd7e7
Roman Link <romanlink@gmx.de>
2024-03-11 18:04:54 +0100

    Update, modify and extend tests for compete_pc

--- tests/testthat/test-compete_pointcloud.R
+++ tests/testthat/test-compete_pointcloud.R
@@ -2,34 +2,41 @@
 
 test_that("compete_pc works for .txt file point clouds", {
   # test basic functionality for cone method
-  expect_equal(
-    length(
-      compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
+  expect_no_error(
+     test1 <- compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
                  tree_source = test_path("testdata", "tree.txt"),
                  comp_method = "cone",
                  print_progress = "none")
-    ),
-    5)
+    )
 
   # test basic functionality for cylinder method
-  expect_equal(
-    length(
-      compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
+  expect_no_error(
+      test2 <- compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
                  tree_source = test_path("testdata", "tree.txt"),
-                 comp_method = "cylinder",
+                 comp_method = "cylinder", center_position = "base_pos",
                  print_progress = "none")
-    ),
-    5)
+    )
 
   # test basic functionality for output of both methods
-  expect_equal(
-    length(
-      compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
+  expect_no_error(
+      test3 <- compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
                  tree_source = test_path("testdata", "tree.txt"),
-                 comp_method = "both",
+                 comp_method = "both", center_position = "base_pos",
                  print_progress = "none")
-    ),
-    7)
+  )
+
+  # test for dimensions
+  expect_equal(c(length(test1), length(test2), length(test3)), c(5,5,7))
+
+  # test if center position is correct
+  expect_equal(test1$center_position, "crown center")
+  expect_equal(test2$center_position, "crown center")
+  expect_equal(test3$center_position, "base")
+
+  # test if different values are returned if different bases are used
+  expect_false(test1$CI_cone == test3$CI_cone)
+  # test if the same values are returned for the same base
+  expect_true(test2$CI_cyl == test3$CI_cyl)
 })
 
 
@@ -54,12 +61,23 @@ test_that("Wrong method in compete_pc fails with an error message", {
 })
 
 test_that("compete_pc works for .txt file point clouds with customized radius", {
-  expect_equal(
-    length(
-      compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
+  # loading works
+  expect_no_error({
+    test <- compete_pc(forest_source = test_path("testdata", "neighborhood.txt"),
                  tree_source = test_path("testdata", "tree.txt"),
                  comp_method = "cylinder", cyl_r = 3,
                  print_progress = "none")
-    ),
-    5)
+    })
+
+  # Output is printed correctly
+  expect_output(
+    print(test),
+    "Point cloud based competition indices for 'tree'"
+  )
+  # no specified center position -> crown center
+  expect_equal(test$center_position, "crown center")
+
+  # correct radius returned
+  expect_equal(test$cyl_r, 3)
 })
+
