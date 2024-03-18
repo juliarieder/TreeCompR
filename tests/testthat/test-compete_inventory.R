a06b5806d59480d31c88806126bc79838255eefe
Roman Link <romanlink@gmx.de>
2024-03-18 19:46:36 +0100

    Add first tests for compete inventory

--- tests/testthat/test-compete_inventory.R
+++ tests/testthat/test-compete_inventory.R
@@ -1,85 +1,56 @@
 # testthat-based unit tests for the functionality of the functions for
 # inventory-based competition indices
 
-test_that("Hegyi index works for inv tables with dbh values", {
-  expect_no_error(
-    test1 <- compete_inv(inv_source = test_path("testdata", "inventory.csv"),
-                 target_source = test_path("testdata", "targettrees_inventory.csv"),
-                 radius = 10, method = "CI_Hegyi", dbh_unit = "m",
-                 height_unit = "m")
-    )
+test_that("Indices work for two different forest_inv datasets", {
+
+  expect_no_error({
+    # read plot dataset
+    plot <- read_inv(test_path("testdata", "inventory.csv"), verbose = FALSE)
+    # read target tree dataset
+    target <- read_inv(test_path("testdata", "targettrees_inventory.csv"),
+                       id = "ID_target")
+    # compute Hegyi
+    test1 <- compete_inv(
+      inv_source = plot,
+      target_source = target,
+      radius = 10,
+      method = "CI_Hegyi",
+      dbh_unit = "m",
+      height_unit = "m")
+  })
+  # compute RK1
+  expect_no_error({
+    test2 <- compete_inv(
+      inv_source = plot,
+      target_source = target,
+      radius = 10,
+      method = "CI_RK1",
+      dbh_unit = "m",
+      height_unit = "m")
+  })
+
+  # compute RK2
+  expect_no_error({
+    test3 <- compete_inv(
+      inv_source = plot,
+      target_source = target,
+      radius = 10,
+      method = "CI_RK2",
+      dbh_unit = "m",
+      height_unit = "m")
+  })
+
+  # compute all dbh-based indices
+  expect_no_error({
+    test4 <- compete_inv(
+      inv_source = plot,
+      target_source = target,
+      radius = 10,
+      method = "all",
+      dbh_unit = "m",
+      height_unit = "m")
+  })
+
+  # test plot output
+  expect_no_error(plot_target(test4))
 })
-
-test_that("CI_RK1 index works", {
-  expect_equal(
-    length(
-      compete_dd(plot_path = test_path("testdata", "inventory.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_inventory.csv"),
-                 radius = 10, method = "CI_RK1",dbh_thr = 0.1)),
-    2)
-})
-
-test_that("CI_RK2 index works", {
-  expect_equal(
-    length(
-      compete_dd(plot_path = test_path("testdata", "inventory.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_inventory.csv"),
-                 radius = 10, method = "CI_RK2",dbh_thr = 0.1)
-    ),
-    2)
-})
-
-
-test_that("all indices at once work with compete_dd()", {
-  expect_equal(
-    length(
-      compete_dd(plot_path = test_path("testdata", "inventory.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_inventory.csv"),
-                 radius = 10, method = "all",dbh_thr = 0.1)
-    ),
-    4)
-})
-
-
-
-test_that("all indices at once for ALS inventory", {
-  expect_equal(
-    ncol(
-      compete_dh(plot_path = test_path("testdata", "inventory_ALS.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_ALS.csv"),
-                 radius = 10, method = "all")
-    ),
-    4)
-})
-
-
-test_that("Braathe index works", {
-  expect_equal(
-    ncol(
-      compete_dh(plot_path = test_path("testdata", "inventory_ALS.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_ALS.csv"),
-                 radius = 10, method = "CI_Braathe")
-    ),
-    2)
-})
-
-test_that("RK3 index works", {
-  expect_equal(
-    ncol(
-      compete_dh(plot_path = test_path("testdata", "inventory_ALS.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_ALS.csv"),
-                 radius = 10, method = "CI_RK3")
-    ),
-    2)
-})
-
-test_that("RK4 index works", {
-  expect_equal(
-    ncol(
-      compete_dh(plot_path = test_path("testdata", "inventory_ALS.csv"),
-                 ttrees_path = test_path("testdata", "targettrees_ALS.csv"),
-                 radius = 10, method = "CI_RK4")
-    ),
-    2)
-})
-
